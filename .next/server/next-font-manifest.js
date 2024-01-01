cd54e32d6c7c83a2d9cc4a2441c41d8f7a08a23a
fischer8 <fischxr@hotmail.com>
2024-01-01 19:52:21 -0300

    next-del

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/f/dev/lavita-next/app/layout\":[\"static/media/3d9ea938b6afa941-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"O8EpV2ib7wscsWc8Tz1Ltwc6BwXtOOpYGEzvNU4QQSk=\"\n}"
