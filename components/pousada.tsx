286bae47a65855c93b73021964be8a2cc2079fa4
fischer8 <fischxr@hotmail.com>
2024-01-03 14:22:07 -0300

    pousada-size

--- components/pousada.tsx
+++ components/pousada.tsx
@@ -15,15 +15,13 @@ export default function Pousada() {
         POUSADA LA VITA
       </h1>
       <hr className='w-[100px] mb-8 mx-auto h-[3px] md:w-[80px] lg:h-[4px] 2xl:mb-16 lg:w-[160px] 2xl:w-[200px] bg-yellow-1000' />
-      <section className='flex w-10/12 mb-8 lg:mb-20 mx-auto'>
+      <section className='flex justify-center w-10/12 mb-8 lg:mb-18 mx-auto'>
         <Image src={frutas} className='w-2/12' alt='frutas'/>
         <Image src={pousada} className='me-2 ms-2 lg:me-8 lg:ms-8 w-8/12' alt='pousada'/>
         <Image src={frutas2} className='w-2/12' alt='frutas2'/>
       </section>
-      <p className='text-center w-10/12 mx-auto'>
+      <p className='text-center mb-4 lg:mb-8 md:text-xl w-10/12 mx-auto'>
         Localizada pertinho das piscinas naturais da Praia de Porto da Rua, a Pousada La Vita é um paraíso: a hospedagem perfeita para você e sua família descansarem sem gastar muito. 
-      </p>
-      <p className='text-center w-10/12 mx-auto'>
         Com ambiente charmoso e acolhedor, oferecemos piscina, café da manhã, culinária regional e muito alto astral. Aqui cada detalhe foi pensado com muito carinho para que você se sinta em casa e tenha uma experiência incrível em São Miguel dos Milagres.
       </p>
     </section>
