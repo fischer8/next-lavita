45729151f60b8051d9f7e18bf707b103a937f975
fischer8 <fischxr@hotmail.com>
2024-01-02 21:00:20 -0300

    dev1

--- components/suites.tsx
+++ components/suites.tsx
@@ -5,7 +5,7 @@ import casal_img from '/public/static/fotos/quartos/casal.png'
 export default function Suites() {
   return (
     <section>
-      <p className='sm:mt-10 text-xs text-center border-black 2xl:text-xl'>
+      <p className='text-xs text-center border-black 2xl:text-xl'>
         POUSADA LA VITA
       </p>
       <h1 className='text-center font-bold text-xl lg:mb-4 2xl:text-4xl'>
@@ -19,11 +19,11 @@ export default function Suites() {
         Todas estão equipadas com smart TV, ar condicionado, internet e frigobar
         e têm vista para o jardim.
       </p>
-      <section className='mb-20 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
-        <Image className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] 2xl:w-[400px]' width={400} src={casal_img} alt='suite-casal' />
+      <section className='sm:mb-16 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
+        <Image className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] sm:h-[250px] 2xl:h-[400px] 2xl:w-[400px]' height={400} width={400} src={casal_img} alt='suite-casal' />
         <section className='flex mx-auto sm:mx-0 sm:ms-4 md:ms-8 w-full flex-col'>
           <h2 className='bg-blue-1000 p-2 ps-2 rounded-b mx-auto sm:mx-0 w-11/12 sm:w-full mb-4 text-white 2xl:text-2xl font-bold'>Suíte casal</h2>
-          <section className='mx-auto w-11/12 sm:mx-0 w-'>
+          <section className='mx-auto w-11/12 sm:mx-0 '>
             <p className='ms-3 text-sm 2xl:text-lg mb-4 2xl:ms-6 '>1 cama queen</p>
             <p className='ms-3 mb-4 2xl:ms-6 2xl:mb-8 text-sm'>Cama extra disponível, mediante pedido</p>
             <section className='ms-3 mb-20 flex w-full flex-wrap 2xl:ms-6'>
@@ -40,11 +40,11 @@ export default function Suites() {
           </section>
         </section>
       </section>
-      <section className='mb-20 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
-        <Image className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] 2xl:w-[400px]' width={400} src={casal_img} alt='suite-casal' />
+      <section className='sm:mb-16 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
+        <Image className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] sm:h-[250px] 2xl:h-[400px] 2xl:w-[400px]' height={400} width={400} src={casal_img} alt='suite-casal' />
         <section className='flex mx-auto sm:mx-0 sm:ms-4 md:ms-8 w-full flex-col'>
           <h2 className='bg-blue-1000 p-2 ps-2 rounded-b mx-auto sm:mx-0 w-11/12 sm:w-full mb-4 text-white 2xl:text-2xl font-bold'>Suíte casal</h2>
-          <section className='mx-auto w-11/12 sm:mx-0 w-'>
+          <section className='mx-auto w-11/12 sm:mx-0 '>
             <p className='ms-3 text-sm 2xl:text-lg mb-4 2xl:ms-6 '>1 cama queen</p>
             <p className='ms-3 mb-4 2xl:ms-6 2xl:mb-8 text-sm'>Cama extra disponível, mediante pedido</p>
             <section className='ms-3 mb-20 flex w-full flex-wrap 2xl:ms-6'>
@@ -61,11 +61,11 @@ export default function Suites() {
           </section>
         </section>
       </section>
-      <section className='mb-20 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
-        <Image loading="lazy" className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] 2xl:w-[400px]' width={400} src={casal_img} alt='suite-casal' />
+      <section className='sm:mb-16 flex flex-col sm:flex-row mx-auto w-5/6 sm:w-[560px] md:w-[700px] 2xl:w-[1000px] justify-between'>
+        <Image className='mx-auto sm:mx-0 w-11/12 sm:w-[250px] sm:h-[250px] 2xl:h-[400px] 2xl:w-[400px]' height={400} width={400} src={casal_img} alt='suite-casal' />
         <section className='flex mx-auto sm:mx-0 sm:ms-4 md:ms-8 w-full flex-col'>
           <h2 className='bg-blue-1000 p-2 ps-2 rounded-b mx-auto sm:mx-0 w-11/12 sm:w-full mb-4 text-white 2xl:text-2xl font-bold'>Suíte casal</h2>
-          <section className='mx-auto w-11/12 sm:mx-0 w-'>
+          <section className='mx-auto w-11/12 sm:mx-0 '>
             <p className='ms-3 text-sm 2xl:text-lg mb-4 2xl:ms-6 '>1 cama queen</p>
             <p className='ms-3 mb-4 2xl:ms-6 2xl:mb-8 text-sm'>Cama extra disponível, mediante pedido</p>
             <section className='ms-3 mb-20 flex w-full flex-wrap 2xl:ms-6'>
