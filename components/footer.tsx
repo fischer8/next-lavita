9c70fa824dbe37f9f2e807fc4764739861aa83bd
fischer8 <fischxr@hotmail.com>
2024-01-11 13:03:06 -0300

    finish

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,12 +10,12 @@ import local from '/public/static/elem/local.png'
 
 export default function Footer() {
   return (
-    <section className='bg-blue-1000 text-white relative w-full pb-32'>
+    <section className='bg-blue-1000 text-white relative w-full pb-24 sm:pb-32'>
       <Image width={300} className='w-32 sm:w-52 2xl:w-[250px] absolute bottom-0 left-0' src={footerLb} alt='folhas' />
       <Image width={300} className='w-32 sm:w-52 2xl:w-[250px] absolute top-0 right-0' src={footerRT} alt='folhas' />
-      <section className='flex items-center flex-wrap z-50 justify-around w-10/12'>
-        <Image className='mt-20' width={200} src={logo_img} alt='logo-lavita' />
-        <section className='flex mt-40 flex-col'>
+      <section className='flex flex-col-reverse justify-center sm:flex-row items-center flex-wrap z-50 sm:justify-around w-10/12'>
+        <Image className='mt-10 sm:mt-20' width={200} src={logo_img} alt='logo-lavita' />
+        <section className='flex mt-10 sm:mt-40 flex-col'>
           <p>POUSADA LA VITA</p>
           <Link href={'#suites'}>Suítes</Link>
           <Link href={'#pousada'}>Sobre a pousada</Link>
@@ -23,7 +23,7 @@ export default function Footer() {
           <Image className='h-14 w-14' src={insta_img} alt='instagram' />
         </section>
         <section>
-          <section className='flex mt-40 gap-2 mb-6'>
+          <section className='flex mt-14 sm:mt-40 gap-2 mb-6'>
             <Image className='h-12 w-12' src={wpp_img} alt='whatsapp' />
             <section>
               <p>11 99435-2770</p>
@@ -39,7 +39,7 @@ export default function Footer() {
             </section>
           </section>
         </section>
-        <button className="mt-40 ">
+        <button className="mt-14 py-2 px-6 rounded bg-yellow-1000 sm:mt-40">
           RESERVE JÁ
         </button>
       </section>
