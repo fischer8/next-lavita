cdf64785428411b4d1ed5810e2a7987287fc192c
fischer8 <fischxr@hotmail.com>
2024-01-12 23:57:29 -0300

    carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -10,16 +10,16 @@ import casal4 from '/public/static/quartos/casal/casal-4.webp'
 import casal5 from '/public/static/quartos/casal/casal-5.webp'
 
 
-export default function Carousel() { 
+export default function Carousel(closeView: (a: boolean) => void) {
   const [page, setPage] = useState(0)
-  const fotos = [casal, casal1,casal2,casal3,casal4,casal5]
-  return ( 
-    <section className='hidden fixed w-10/12 inset-0 h-fit mx-auto top-1/4 bg-white'>
-    <section className='w-1/3'>
-      <Image src={fotos[page]} alt='fotos' className='bg-red-400'/>
-      <button disabled={page === 0} onClick={() => setPage((prev) => prev - 1)} className='absolute left-4'>{'<'}</button>
-      <button disabled={page === 5} onClick={() => setPage((prev) => prev + 1)} className='absolute right-4'>{'>'}</button>
-    </section>
+  const fotos = [casal, casal1, casal2, casal3, casal4, casal5]
+  return (
+    <section onClick={() => closeView(true)} className='hidden fixed top-0 w-full h-full bg-opacity-50 bg-black'>
+      <section className='fixed w-1/3 inset-0 h-fit mx-auto my-auto bg-white'>
+        <Image src={fotos[page]} alt='fotos' className='bg-red-400' />
+        <button disabled={page === 0} onClick={() => setPage((prev) => prev - 1)} className='absolute left-4'>{'<'}</button>
+        <button disabled={page === 5} onClick={() => setPage((prev) => prev + 1)} className='absolute right-4'>{'>'}</button>
+      </section>
     </section>
   );
 }
