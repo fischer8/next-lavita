56176c5a82407a52c3ea067a4ab479a1061aaa48
fischer8 <fischxr@hotmail.com>
2024-01-13 12:40:06 -0300

    dev12345

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -15,7 +15,7 @@ const Carousel: React.FC<CarouselProps> = ({fotos, page, setPage, setView}) => {
       <section className='absolute z-50 w-11/12 sm:w-8/12 md:w-1/2 lg:w-1/3 inset-0 h-fit mx-auto my-auto bg-white'>
         <section className='relative'>
           <Image src={fotos[page]} alt='fotos' className='' />
-          <button onClick={() => setView([])} className='absolute top-4 right-4 bg-white p-4'>X</button>
+          <button onClick={() => setView([],0)} className='absolute top-4 right-4 bg-white p-4'>X</button>
           <button className='absolute p-2 bg-white top-1/2 left-4' disabled={page === 0} onClick={() => setPage((prev) => prev - 1)} >{'<'}</button>
           <button className='absolute p-2 bg-white top-1/2 right-4' disabled={page === (fotos.length - 1)} onClick={() => setPage((prev) => prev + 1)} >{'>'}</button>
         </section>
