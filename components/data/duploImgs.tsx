ff233c9c681762b7b2d20a9ba708f49685bc25d3
fischer8 <fischxr@hotmail.com>
2024-01-16 11:35:14 -0300

    dev

--- components/data/duploImgs.tsx
+++ components/data/duploImgs.tsx
@@ -6,8 +6,7 @@ import d5 from '/public/static/quartos/duplo/d5.webp'
 import d6 from '/public/static/quartos/duplo/d6.webp'
 import d7 from '/public/static/quartos/duplo/d7.webp'
 import d8 from '/public/static/quartos/duplo/d8.webp'
-import d9 from '/public/static/quartos/duplo/d9.webp'
 
-const duploImgs = [d1,d2,d3,d4,d5,d6,d7,d8,d9]
+const duploImgs = [d1,d2,d3,d4,d5,d6,d7,d8]
 export default duploImgs;
 
--- components/data/triploImgs.tsx
+++ components/data/triploImgs.tsx
@@ -1,8 +1,6 @@
 import t1 from '/public/static/quartos/triplo/t1.webp'
-import t2 from '/public/static/quartos/triplo/t2.webp'
-import t3 from '/public/static/quartos/triplo/t3.webp'
 import t4 from '/public/static/quartos/triplo/t4.webp'
 
-const triploImgs = [t1,t2,t3,t4]
+const triploImgs = [t1,t4]
 export default triploImgs;
 
