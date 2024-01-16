fe9e1f5968f0e6bc1508d60c901facb2771fe31c
fischer8 <fischxr@hotmail.com>
2024-01-16 12:40:10 -0300

    dev

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import Image, { StaticImageData } from 'next/image';
-import {string, number, func, arrayOf, shape} from 'prop-types';
+import { string, number, func, arrayOf, shape } from 'prop-types';
 
 interface CarouselProps {
   fotos: StaticImageData[];
@@ -14,11 +14,13 @@ const Carousel: React.FC<CarouselProps> = React.memo(({ fotos, page, setPage, se
   const decreasePage = React.useMemo(() => () => setPage((prev: number) => prev - 1), [setPage]);
   const increasePage = React.useMemo(() => () => setPage((prev: number) => prev + 1), [setPage]);
   return (
-    <section className='select-none fixed flex items-center justify-center top-0 w-full h-full bg-opacity-90 bg-black'>
-      <section className='w-fit inset-0 h-fit mx-auto my-auto rounded'>
-        <section className='relative object-contain'>
-          <Image loading='eager' placeholder='blur' src={fotos[page]} alt='fotos' className='z-50 rounded mx-auto' />
-          <button onClick={() => setView([], 0)} className='absolute rounded top-1 right-1 bg-white px-3 py-1'>X</button>
+  <section className='select-none fixed flex items-center justify-center top-0 w-full h-full bg-opacity-90 bg-black'>
+    <section className='max-w-[600px]'>
+      <section className='relative overflow-hidden'>
+        <section className='flex transition-transform ease-out duration-200' style={{ transform: `translateX(-${page * 100}%)`}}>
+          {fotos.map((foto, i: number) => <Image className='rounded' key={`c${i}`} loading='eager' placeholder='blur' src={foto} alt='fotos' />)}
+        </section>
+        <button onClick={() => setView([], 0)} className='absolute rounded top-1 right-1 bg-white px-3 py-1'>X</button>
           <section className='absolute flex justify-center w-fit bottom-2 inset-x-0 mx-auto'>
             <button className='rounded border px-4 sm:p-2 sm:px-6 disabled:opacity-35  bg-white '
               onClick={decreasePage}
@@ -31,9 +33,9 @@ const Carousel: React.FC<CarouselProps> = React.memo(({ fotos, page, setPage, se
               {'>'}
             </button>
           </section>
-        </section>
       </section>
     </section>
+  </section>
   );
 })
 
