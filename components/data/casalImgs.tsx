1219a9b578227b9b8fedc86fab03b42a6e283e54
fischer8 <fischxr@hotmail.com>
2024-01-18 21:29:35 -0300

    dev

--- components/data/casalImgs.tsx
+++ components/data/casalImgs.tsx
@@ -2,12 +2,11 @@ import c1 from '/public/static/quartos/casal/c1.webp'
 import c2 from '/public/static/quartos/casal/c2.webp'
 import c3 from '/public/static/quartos/casal/c3.webp'
 import c4 from '/public/static/quartos/casal/c4.webp'
-import c5 from '/public/static/quartos/casal/c5.webp'
 import c6 from '/public/static/quartos/casal/c6.webp'
 import c7 from '/public/static/quartos/casal/c7.webp'
 import c8 from '/public/static/quartos/casal/c8.webp'
 import c9 from '/public/static/quartos/casal/c9.webp'
 
-const casalImgs = [c1,c2,c3,c4,c5,c6,c7,c8,c9]
+const casalImgs = [c1,c2,c3,c4,c6,c7,c8,c9]
 export default casalImgs;
 
